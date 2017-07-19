e37371a457ba2be6ab13be7df7a28fb8bd12f3fe
chfw <wangc_2011@hotmail.com>
2017-07-19 20:00:13 +0100

    apply moban to itself

--- setup.py
+++ setup.py
@@ -1,36 +1,102 @@
-from setuptools import setup, find_packages
-
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+from platform import python_implementation
+import sys
+PY2 = sys.version_info[0] == 2
+PY26 = PY2 and sys.version_info[1] < 7
 
 NAME = 'moban'
-AUTHOR = 'C.W.'
+AUTHOR = 'C. W.'
 VERSION = '0.0.7'
-EMAIL = "wangc_2011 (at) hotmail.com"
+EMAIL = 'wangc_2011@hotmail.com'
 LICENSE = 'MIT'
 ENTRY_POINTS = {
     'console_scripts': [
-        '%s = moban.main:main' % NAME
+        'moban = moban.main:main'
     ]
 }
-PACKAGES = find_packages(exclude=['ez_setup', 'examples', 'tests'])
-DESCRIPTION = 'Yet another jinja2 cli command for static text generation'
-INSTALL_REQUIRES = ['pyyaml>=3.11', 'jinja2>=2.7.1']
+DESCRIPTION = (
+    'Yet another jinja2 cli command for static text generation' +
+    ''
+)
+URL = 'https://github.com/moremoban/moban'
+DOWNLOAD_URL = '%s/archive/0.0.7.tar.gz' % URL
+FILES = ['README.rst', 'CHANGELOG.rst']
+KEYWORDS = [
+    'jinja2',
+    'moban',
+    'python'
+]
+
 CLASSIFIERS = [
-    'Development Status :: 3 - Alpha',
-    'License :: OSI Approved :: MIT License',
+    'Topic :: Office/Business',
+    'Topic :: Utilities',
+    'Topic :: Software Development :: Libraries',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: BSD License',
     'Intended Audience :: Developers',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+]
+
+INSTALL_REQUIRES = [
+    'pyyaml>=3.11',
+    'jinja2>=2.7.1',
 ]
 
 
+PACKAGES = find_packages(exclude=['ez_setup', 'examples', 'tests'])
+EXTRAS_REQUIRE = {
+}
+
+
 def read_files(*files):
+    """Read files into setup"""
     text = ""
     for single_file in files:
-        text = text + read(single_file) + "\n"
+        content = read(single_file)
+        text = text + content + "\n"
     return text
 
 
 def read(afile):
+    """Read a file into setup"""
     with open(afile, 'r') as opened_file:
-        return opened_file.read()
+        content = filter_out_test_code(opened_file)
+        content = "".join(list(content))
+        return content
+
+
+def filter_out_test_code(file_handle):
+    found_test_code = False
+    for line in file_handle.readlines():
+        if line.startswith('.. testcode:'):
+            found_test_code = True
+            continue
+        if found_test_code is True:
+            if line.startswith('  '):
+                continue
+            else:
+                empty_line = line.strip()
+                if len(empty_line) == 0:
+                    continue
+                else:
+                    found_test_code = False
+                    yield line
+        else:
+            for keyword in ['|version|', '|today|']:
+                if keyword in line:
+                   break
+            else:
+                yield line
 
 
 if __name__ == '__main__':
@@ -40,13 +106,17 @@ if __name__ == '__main__':
         version=VERSION,
         author_email=EMAIL,
         description=DESCRIPTION,
+        url=URL,
+        download_url=DOWNLOAD_URL,
+        long_description=read_files(*FILES),
+        license=LICENSE,
+        keywords=KEYWORDS,
+        extras_require=EXTRAS_REQUIRE,
+        tests_require=['nose'],
         install_requires=INSTALL_REQUIRES,
         packages=PACKAGES,
         include_package_data=True,
-        long_description=read_files('README.rst', 'CHANGELOG.rst'),
         zip_safe=False,
-        tests_require=['nose'],
-        license=LICENSE,
-        classifiers=CLASSIFIERS,
-        entry_points=ENTRY_POINTS
+        entry_points=ENTRY_POINTS,
+        classifiers=CLASSIFIERS
     )
