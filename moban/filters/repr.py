ffe23433056e0a710f81bb22a9161cdaf3ff2c12
chfw <wangc_2011@hotmail.com>
2018-06-01 07:42:14 +0100

    :bug: fix format call for python 2.6

--- moban/filters/repr.py
+++ moban/filters/repr.py
@@ -4,6 +4,6 @@ from moban.extensions import JinjaFilter
 @JinjaFilter('repr')
 def repr_function(string):
     if isinstance(string, list):
-        return ["'{}'".format(str(element)) for element in string]
+        return ["'{0}'".format(str(element)) for element in string]
     else:
-        return "'{}'".format(str(string))
+        return "'{0}'".format(str(string))
