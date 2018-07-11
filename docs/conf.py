7dc4b44fa835bd632722f1f8d397602d516172ee
chfw <wangc_2011@hotmail.com>
2018-07-11 18:40:52 +0100

    :books: pump version number

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ author = u'C. W.'
 # The short X.Y version
 version = u'0.2.3'
 # The full version, including alpha/beta/rc tags
-release = u'0.2.3'
+release = u'0.2.4'
 
 
 # -- General configuration ---------------------------------------------------
