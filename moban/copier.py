13c7071cb0016dc5011082bfb9a9c5ef2f2adac3
chfw <wangc_2011@hotmail.com>
2018-07-12 08:31:59 +0100

    :hammer: code refactoring

--- moban/copier.py
+++ moban/copier.py
@@ -14,14 +14,13 @@ class Copier(object):
 
     def copy_files(self, file_list):
         for dest, src in _iterate_list_of_dicts(file_list):
-            self._file_count += 1
             src_path = self._get_src_file(src)
             if src_path is None:
                 if src.endswith('**'):
                     source_dir = src[:-3]
                     src_path = self._get_src_file(source_dir)
                     if src_path:
-                        self._do_copy_dir_recursively(src[:-3], src_path, dest)
+                        self._copy_dir_recursively(src[:-3], src_path, dest)
                     else:
                         reporter.report_error_message(
                             "{0} cannot be found".format(source_dir)
@@ -31,9 +30,10 @@ class Copier(object):
                         "{0} cannot be found".format(src)
                     )
             elif os.path.isdir(src_path):
-                self._do_copy_dir(src, src_path, dest)
+                self._copy_dir(src, src_path, dest)
             elif HASH_STORE.are_two_file_different(src_path, dest):
-                self._do_copy(src_path, dest)
+                self._increment_file_count()
+                self._copy(src_path, dest)
 
     def number_of_copied_files(self):
         return self._count
@@ -52,11 +52,12 @@ class Copier(object):
         else:
             return None
 
-    def _do_copy_dir(self, source, actual_source_path, dest):
+    def _copy_dir(self, source, actual_source_path, dest):
         new_file_pair = []
         for file_name in os.listdir(actual_source_path):
             real_src_file = os.path.join(actual_source_path, file_name)
             if os.path.isfile(real_src_file):
+                self._increment_file_count()
                 src_file_under_dir = os.path.join(source, file_name)
                 dest_file_under_dir = os.path.join(dest, file_name)
                 new_file_pair.append(
@@ -65,9 +66,10 @@ class Copier(object):
         if len(new_file_pair) > 0:
             self.copy_files(new_file_pair)
 
-    def _do_copy_dir_recursively(self, source, actual_source_path, dest):
+    def _copy_dir_recursively(self, source, actual_source_path, dest):
         new_file_pair = []
         for file_name in os.listdir(actual_source_path):
+            self._increment_file_count()
             src_file_under_dir = os.path.join(source, file_name)
             dest_file_under_dir = os.path.join(dest, file_name)
             new_file_pair.append(
@@ -75,13 +77,16 @@ class Copier(object):
             )
         self.copy_files(new_file_pair)
 
-    def _do_copy(self, src_path, dest):
+    def _copy(self, src_path, dest):
         dest_folder = os.path.dirname(dest)
         if dest_folder:
             utils.mkdir_p(dest_folder)
         reporter.report_copying(src_path, dest)
         shutil.copy(src_path, dest)
         self._count = self._count + 1
+
+    def _increment_file_count(self):
+        self._file_count += 1
 
 
 def _iterate_list_of_dicts(list_of_dict):
