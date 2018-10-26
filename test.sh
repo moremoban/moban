044cd2f6c3c9188a79ac5de0370291e3368c6ddd
chfw <wangc_2011@hotmail.com>
2018-10-26 22:30:28 +0100

    :fire: ignore e203 but inherit other defaults

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ pip freeze
 cd tests/moban-mako
 python setup.py install
 cd ../../
-nosetests --with-cov --with-doctest --doctest-extension=.rst --cover-package moban --cover-package tests && flake8 . --ignore=E203 --exclude=.moban.d
+nosetests --with-cov --with-doctest --doctest-extension=.rst --cover-package moban --cover-package tests && flake8 . --exclude=.moban.d --ignore=E203,E121,E123,E126,E226,E24,E704,W503,W504
