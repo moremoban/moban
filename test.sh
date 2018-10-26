f4688fadca11bd98c4120b8972583156fa82a035
chfw <wangc_2011@hotmail.com>
2018-10-26 22:06:57 +0100

    :art: ignore E203, https://github.com/ambv/black/issues/544

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ pip freeze
 cd tests/moban-mako
 python setup.py install
 cd ../../
-nosetests --with-cov --with-doctest --doctest-extension=.rst --cover-package moban --cover-package tests && flake8 . --exclude=.moban.d
+nosetests --with-cov --with-doctest --doctest-extension=.rst --cover-package moban --cover-package tests && flake8 . --ignore=E203 --exclude=.moban.d
