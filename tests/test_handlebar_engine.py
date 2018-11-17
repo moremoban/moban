30791312be22a06ca745d4e2e3f0c14eb03a3b66
chfw <wangc_2011@hotmail.com>
2018-11-17 11:43:28 +0000

    :micrscope: test newly create class interfaces

---
+++ tests/test_handlebar_engine.py
@@ -0,0 +1,14 @@
+import os
+
+from nose.tools import eq_
+from moban.jinja2.engine import Engine
+
+
+def test_handlebars_template_not_found():
+    path = os.path.join("tests", "fixtures", "jinja_tests")
+    engine = Engine([path])
+    template = engine.get_template("file_tests.template")
+    data = dict(test="here")
+    result = engine.apply_template(template, data, None)
+    expected = "yes\nhere".encode("utf-8")
+    eq_(expected, result)
