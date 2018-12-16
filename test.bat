632a2bb167fb25e3ac569d28cf25a3c0ef39ac86
chfw <wangc_2011@hotmail.com>
2018-12-16 19:52:02 +0000

    :green_heart: fix exclude patterns

--- test.bat
+++ test.bat
@@ -4,4 +4,4 @@ cd tests\moban-mako
 python setup.py install
 cd ..\..\
 nosetests --with-coverage --cover-package=moban --cover-package=tests
-flake8 . --exclude=docs --exclude=.moban.d --ignore=E203,E121,E123,E126,E226,E24,E704,W503,W504
+flake8 . --exclude=docs,.moban.d --ignore=E203,E121,E123,E126,E226,E24,E704,W503,W504
