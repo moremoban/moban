ca1489e1fca85f52a53fc1bd1f69938879752598
chfw <wangc_2011@hotmail.com>
2019-01-06 14:07:26 +0000

    :microscope: test incorrrect data file intput

--- tests/test_context.py
+++ tests/test_context.py
@@ -27,3 +27,12 @@ def test_json_data_overrides_environ_variables():
     context = Context(os.path.join("tests", "fixtures"))
     data = context.get_data("simple.json")
     eq_(data[test_var], test_value)
+
+
+def test_unknown_data_file():
+    test_var = "TEST_ENVIRONMENT_VARIABLE"
+    test_value = "am I found"
+    os.environ[test_var] = test_value
+    context = Context(os.path.join("tests", "fixtures"))
+    data = context.get_data("unknown.data")
+    eq_(data[test_var], test_value)
