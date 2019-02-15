bfed59c0fccc29e6a845d17590c2ae169d33dd91
chfw <wangc_2011@hotmail.com>
2019-02-15 21:03:04 +0000

    :microscope: more tests

--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -13,6 +13,13 @@ def test_template_target_repr():
     eq_("template_file,dat_file,output,jinja2", repr(require))
 
 
+def test_template_target_output_suffix_change():
+    require = TemplateTarget(
+        "template_file", "dat_file", "output.copy", template_type="copy"
+    )
+    eq_("template_file,dat_file,output,copy", repr(require))
+
+
 def test_clone_params():
     require = GitRequire(git_url="http://github.com/some/repo")
     actual = require.clone_params()
