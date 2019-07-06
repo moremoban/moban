b2358e0c3f5be7f804f862e1f6f52fb2ebea65bc
chfw <wangc_2011@hotmail.com>
2019-07-06 12:12:38 +0100

    bump the version

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,8 @@ DESCRIPTION = (
 # -- Project information -----------------------------------------------------
 
 project = 'moban'
-copyright = '2017-2019 Onni Software Ltd. and its contributors'
-author = 'Onni Software Ltd.'
+copyright = '2017-2019 Onni Software Ltd.'
+author = 'C. W.'
 # The short X.Y version
 version = '0.4.5'
 # The full version, including alpha/beta/rc tags
--- moban/__init__.py
+++ moban/__init__.py
@@ -1,3 +1,4 @@
-from moban._version import __author__, __version__
+from moban._version import __version__
+from moban._version import __author__
 
 __all__ = ("__author__", "__version__")
