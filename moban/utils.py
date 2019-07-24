2481464fc5e38026fbcad0e35f23bc78679c162d
chfw <wangc_2011@hotmail.com>
2019-07-24 23:25:52 +0100

    :green_heart: use norm path over norm case

--- moban/utils.py
+++ moban/utils.py
@@ -107,7 +107,7 @@ def get_template_path(template_dirs, template):
         ) and moban_fs.is_file(fs.path.join(a_dir, template))
 
         if template_file_exists:
-            return os.path.normcase(os.path.join(
+            return os.path.normpath(os.path.join(
                 os.getcwd(), a_dir, template))
     raise exceptions.FileNotFound
 
