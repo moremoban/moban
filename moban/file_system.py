08ec831752a4a9f31e23671c200c7655195d31ec
chfw <wangc_2011@hotmail.com>
2019-07-27 12:00:09 +0100

    :hammer: use u for unicode convertion and :microscope: log errors from pyfilesystem2

--- moban/file_system.py
+++ moban/file_system.py
@@ -1,14 +1,30 @@
 import sys
+import logging
 from contextlib import contextmanager
+
+from moban import reporter
 
 import fs
 import fs.path
 
 PY2 = sys.version_info[0] == 2
-
+LOG = logging.getLogger(__name__)
 
 path_join = fs.path.join
 path_splitext = fs.path.splitext
+
+
+def log_fs_failure(function_in_this_module):
+    def wrapper(*args, **kwds):
+        try:
+            return function_in_this_module(*args, **kwds)
+        except fs.errors.CreateFailed:
+            message = "Failed to open %s" % args[0]
+            LOG.debug(message)
+            reporter.report_error_message(message)
+            raise
+
+    return wrapper
 
 
 @contextmanager
@@ -78,16 +94,17 @@ def list_dir(path):
             yield file_name
 
 
+@log_fs_failure
 def abspath(path):
     path = to_unicode(path)
     dir_name = fs.path.dirname(path)
     the_file_name = fs.path.basename(path)
+
     with fs.open_fs(dir_name) as the_fs:
         return the_fs.getsyspath(the_file_name)
 
 
 def to_unicode(path):
-    if PY2:
-        if isinstance(path, unicode) is False:
-            return unicode(path)
+    if PY2 and path.__class__.__name__ != "unicode":
+        return u"".__class__(path)
     return path
