a38e14124683fbd9542352160e0626f63b9845f9
chfw <wangc_2011@hotmail.com>
2019-07-28 22:26:32 +0100

    :bug: inverse the logic for making windows dirs

--- moban/utils.py
+++ moban/utils.py
@@ -76,7 +76,7 @@ def write_file_out(filename, content):
     if PY2 and content.__class__.__name__ == "unicode":
         content = content.encode("utf-8")
 
-    if "zip://" not in filename or "tar://" not in filename:
+    if "zip://" not in filename and "tar://" not in filename:
         # fix me
         dest_folder = os.path.dirname(filename)
         if dest_folder:
