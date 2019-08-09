8a57815242bca238e027ba1594ea2a2b55fd532a
chfw <wangc_2011@hotmail.com>
2019-08-09 18:46:57 +0100

    :green_heart: update hash store unit test to be windows friendly

--- tests/test_hash_store.py
+++ tests/test_hash_store.py
@@ -7,7 +7,9 @@ from moban.hashstore import HashStore
 
 class TestHashStore:
     def setUp(self):
-        self.source_template = os.path.join("tests", "fixtures", "a.jj2")
+        self.source_template = os.path.normcase(
+            os.path.join("tests", "fixtures", "a.jj2")
+        )
         self.fixture = (
             "test.out",
             "test content".encode("utf-8"),
