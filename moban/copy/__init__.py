6ea5cb822a3b45afad8f8c85e1b92768cc9c65bc
chfw <wangc_2011@hotmail.com>
2019-08-11 17:35:31 +0100

    :shirt: coding style update

--- moban/copy/__init__.py
+++ moban/copy/__init__.py
@@ -23,7 +23,9 @@ class ContentForwardEngine(object):
         self.template_fs = template_fs
 
     def get_template(self, template_file):
-        return self.template_fs.readbytes(file_system.to_unicode(template_file))
+        return self.template_fs.readbytes(
+            file_system.to_unicode(template_file)
+        )
 
     def get_template_from_string(self, string):
         return string
