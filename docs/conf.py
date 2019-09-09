c62ea15c4463001290777f10ddabfbbdf2e244a8
chfw <wangc_2011@hotmail.com>
2019-09-09 17:58:29 +0100

    :egg: :ferris_wheel: release 0.6.0 - PyFileSystem2 Support

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'C. W.'
 # The short X.Y version
 version = '0.6.0'
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.6.0'
 
 # -- General configuration ---------------------------------------------------
 
