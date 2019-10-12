e04f1aaf807a3318f45ff3698f067973261e2e94
chfw <wangc_2011@hotmail.com>
2019-10-12 16:08:09 +0100

    :hammer: code refactoring

--- moban/core/mobanfile/store.py
+++ moban/core/mobanfile/store.py
@@ -1,14 +1,3 @@
-class Store:
-    def __init__(self):
-        self.init()
-
-    def add(self, target):
-        self.targets.append(target)
-        self.look_up_by_output[target.output] = target
-
-    def init(self):
-        self.targets = []
-        self.look_up_by_output = {}
-
+from moban.core.definitions import Store
 
 STORE = Store()
