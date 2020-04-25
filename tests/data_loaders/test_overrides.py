78b4b0420e183864ca9755e847b1921ddf7aa771
chfw <wangc_2011@hotmail.com>
2020-04-25 15:10:58 +0100

    :bug: fix regression caused by mobanfile inheritance! resolves #365

--- tests/data_loaders/test_overrides.py
+++ tests/data_loaders/test_overrides.py
@@ -18,6 +18,21 @@ def test_overrides_a_list_of_config_files():
     for item, expected_item in zip(actual.items(), expected):
         eq_(item, expected_item)
 
+    eq_(len(actual), len(expected))
+
+
+def test_overrides_a_list_of_config_files_but_cannot_find_them():
+    base_dir = os.path.join("tests", "fixtures", "issue_126")
+    actual = load_data(None, os.path.join(base_dir, "the_config.yaml"))
+
+    expected = [
+        ("key", "value"),
+    ]
+    for item, expected_item in zip(actual.items(), expected):
+        eq_(item, expected_item)
+
+    eq_(len(actual), len(expected))
+
 
 def test_overrides_ignores_override_sequence():
     base_dir = os.path.join("tests", "fixtures", "issue_126")
