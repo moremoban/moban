98bf8de75647fbe0bfb9a1b4d0751d584ea6084d
chfw <wangc_2011@hotmail.com>
2020-05-21 23:02:48 +0100

    :egg: :ferris_wheel: release 0.7.5

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ project = 'moban'
 copyright = '2017-2020 Onni Software Ltd.'
 author = 'C. W.'
 # The short X.Y version
-version = '0.7.4'
+version = '0.7.5'
 # The full version, including alpha/beta/rc tags
-release = '0.7.4'
+release = '0.7.5'
 
 # -- General configuration ---------------------------------------------------
 
