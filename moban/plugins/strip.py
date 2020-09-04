f6a0dcd41abcc35ea087a9081f3db27a90fb7086
chfw <wangc_2011@hotmail.com>
2020-09-04 22:14:22 +0100

    :bug: update strip content processor

--- moban/plugins/strip.py
+++ moban/plugins/strip.py
@@ -2,6 +2,6 @@ from moban.core.content_processor import ContentProcessor
 
 
 @ContentProcessor("strip", "Stripping", "Stripped")
-def strip(content: str) -> str:
+def strip(content: str, _: dict) -> str:
     """Works like 'copy', but strip empty spaces before and after"""
     return content.strip()
